45b26378c6fd0ddfddd083562ebdfb03ee96bfe5
vaibhav rangarajan <rvaibhav1793@gmail.com>
2018-06-25 08:57:53 -0400

    Updated secure http requests

--- src/app/services/search.service.client.ts
+++ src/app/services/search.service.client.ts
@@ -6,7 +6,7 @@ export class SearchServiceClient {
   constructor(private ajax: Http) {}
   searchCraigs(search, category) {
     console.log("In client service:"+search);
-    let url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20craigslist.search%20where%20location%3D%22boston%22%20and%20type%3D%22'+category+'%22%20and%20query%3D%22'+search+'%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+    let url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20craigslist.search%20where%20location%3D%22boston%22%20and%20type%3D%22'+category+'%22%20and%20query%3D%22'+search+'%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
     return this.ajax.get(url)
       .map((res: Response) => {
         var temp = res.json();
